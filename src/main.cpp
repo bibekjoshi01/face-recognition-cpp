6a2eea3be2e38128e0c4cc4b5c86ba658c30f9dc
bibekjoshi01 <bibekjoshi34@gmail.com>
2024-01-02 23:13:18 +0545

    image

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,13 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
 #include <fstream>
-#include "imgui.h"
+#include <ctime>
 
 using namespace std;
 using namespace cv;
 
 bool captureImage = false;
 Mat capturedImage;
-string enteredName;
 
 void onMouse(int event, int x, int y, int flags, void *userdata)
 {
@@ -34,19 +33,6 @@ void detectEyes(Mat &img, Rect face, CascadeClassifier &eyes_cascade)
     }
 }
 
-void promptForName(int event, int x, int y, int flags, void *userdata)
-{
-    if (event == EVENT_LBUTTONDOWN)
-    {
-        // Display a prompt window for entering the name
-        enteredName = cv::getWindowProperty("EnterName", WND_PROP_AUTOSIZE) == WINDOW_NORMAL ? "" : enteredName;
-        enteredName += cv::waitKey(0);
-
-        putText(capturedImage, enteredName, Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
-        imshow("Camera", capturedImage);
-    }
-}
-
 int main()
 {
     namedWindow("Camera");
@@ -56,7 +42,7 @@ int main()
 
     if (!video.isOpened())
     {
-        cout << "Error: Couldn't open the camera." << endl;
+        std::cout << "Error: Couldn't open the camera." << endl;
         return -1;
     }
 
@@ -65,7 +51,7 @@ int main()
     if (!facedetect.load("C:/Users/Bibek Joshi/Desktop/ImageRecognition/data/haarcascades/lbpcascade_frontalface_improved.xml") ||
         !eyes_cascade.load("C:/Users/Bibek Joshi/Desktop/ImageRecognition/data/haarcascades/haarcascade_eye.xml"))
     {
-        cout << "Error: Couldn't load one or more cascade classifiers." << endl;
+        std::cout << "Error: Couldn't load one or more cascade classifiers." << endl;
         return -1;
     }
 
@@ -79,7 +65,7 @@ int main()
         // Check if the frame is empty
         if (img.empty())
         {
-            cout << "Error: Couldn't read frame from the camera." << endl;
+            std::cout << "Error: Couldn't read frame from the camera." << endl;
             break;
         }
 
@@ -88,34 +74,30 @@ int main()
             // Copy the image for display in the prompt window
             capturedImage = img.clone();
 
-            // Display a window to prompt the user to enter a name
-            namedWindow("EnterName");
-            setMouseCallback("EnterName", promptForName, NULL);
+            // Get the current time
+            std::time_t now = std::time(nullptr);
 
-            // Wait for the user to enter a name
-            imshow("EnterName", capturedImage);
-            waitKey(0);
+            // Convert to local time
+            std::tm ltm;
+            localtime_s(&ltm, &now); // Correct usage of localtime_s
+
+            // Create a timestamp string
+            char timestamp[20];
+            std::strftime(timestamp, sizeof(timestamp), "%Y%m%d%H%M%S", &ltm);
 
             // Save image
-            string imageName = "C:/Users/Bibek Joshi/Desktop/ImageRecognition/images/" + enteredName + "_captured_photo.jpg";
+            string imageName = "C:/Users/Bibek Joshi/Desktop/ImageRecognition/images/captured_photo_" + string(timestamp) + ".jpg";
             imwrite(imageName, capturedImage);
-            cout << "Photo captured and saved as " << imageName << endl;
+            std::cout << "Photo captured and saved as " << imageName << endl;
 
-            // Save information to CSV file
-            ofstream csvFile("C:/Users/Bibek Joshi/Desktop/ImageRecognition/images/data.csv", ios::app);
-            csvFile << enteredName << "," << imageName << endl;
-            csvFile.close();
-
-            // Close the prompt window
-            destroyWindow("EnterName");
-
+            // Reset the capture flag
             captureImage = false;
         }
 
         vector<Rect> faces;
         facedetect.detectMultiScale(img, faces, 1.3, 5);
 
-        cout << faces.size() << " face(s) found." << endl;
+        std::cout << faces.size() << " face(s) found." << endl;
 
         for (const Rect &face : faces)
         {
